fe1761e6c52a0e4a881b89ac891fb989ebd2feff
ckost9n <ckost9n@gmail.com>
2021-09-26 11:58:38 +0300

    Initial Commit

---
+++ FavoritePlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoritePlaces
+//
+//  Created by Konstantin on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
